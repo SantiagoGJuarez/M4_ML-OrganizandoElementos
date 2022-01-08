074e2a8070596faf3c66bf1baf27939dc2a14f45
Santiago Juarez <santiagogjuarez@gmail.com>
2022-01-07 23:20:18 -0300

    Upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.listen(5000, () => {
+    console.log('Servidor corriendo en el puerto 5000')
+})
